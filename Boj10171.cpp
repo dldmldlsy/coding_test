a7eb726bb48f09401977a096a6a04d3e5bd71685
dldmldlsy <dldmldlsy@naver.com>
2021-06-21 15:09:57 +0900

    백준 10171번

---
+++ Boj10171.cpp
@@ -0,0 +1,13 @@
+//백준 10171번 21.06.21
+//단계별 입출력과 사칙연산
+//correct! 
+
+//\출력하려면 \\써줘야함. 
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "\\    /\\"<<endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+}
