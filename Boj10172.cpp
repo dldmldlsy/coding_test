2444401340ed0a781f234ad7be42a31e667b8891
dldmldlsy <dldmldlsy@naver.com>
2021-06-22 11:09:20 +0900

    백준 10172번

---
+++ Boj10172.cpp
@@ -0,0 +1,16 @@
+//백준 10172번 
+//알고리즘 단계별 입출력과 사칙연산
+//개 그림 출력하기 
+
+// 큰따옴표출력시 앞에 \붙이기= \"
+
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout<<"( 0 )\"\"\"\\"<<endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+
+}
