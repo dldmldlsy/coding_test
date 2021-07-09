1c9cc3970867609a0fe3e77e7528a62d0b46cac4
dldmldlsy <dldmldlsy@naver.com>
2021-07-09 23:04:11 +0900

    프린터큐

---
+++ 백준/Boj1966.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+#include<queue>
+#include<iostream>
+using namespace std;
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    queue<int> q;
+    vector<int> v;
+    for (int i = 0; i < priorities.size(); i++) {
+        q.push(priorities[i]);
+        v.push_back(priorities[i]);
+    }
+    int i = 1;
+    int count = 0;
+    while (!q.empty()) {
+        int now = q.front();
+        q.pop();
+        bool printer = true;
+        for (int j = i; j < v.size(); j++)
+        {
+            if (now < v[j]) {
+                q.push(now);
+                v.push_back(now);
+                printer = false;
+                if (i - 1 == location) location = v.size() - 1;
+                break;
+            }
+        }
+        if (printer)
+        {
+           // cout << now << " "; 
+            count++;
+            if (i - 1 == location) {
+                answer = count;
+            }
+        }
+        i++;
+    }
+    return answer;
+}
+int main() {
+    int t; 
+    cin >> t;
+    int n, m;
+    while (t--) {
+        cin >> n >> m;
+        vector<int> v;
+        int x;
+        while (n--) {
+            cin >> x;
+            v.push_back(x);
+        }
+        cout<<solution(v, m) << "\n";
+    }
+}
