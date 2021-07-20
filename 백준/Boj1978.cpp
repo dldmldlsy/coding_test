42ddee750aa4a612b0c576ee405fa99f7d63ea20
dldmldlsy <dldmldlsy@naver.com>
2021-07-20 22:21:27 +0900

    백준 소수찾기

---
+++ 백준/Boj1978.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+bool isPrimeNumber(int x);
+int main() {
+	int n; 
+	cin >> n; int x; int count = 0;
+	while (n--) {
+		cin >> x;
+		if (isPrimeNumber(x))
+			count++;
+	}
+	cout << count;
+
+}
+bool isPrimeNumber(int x) {
+	if (x <= 1)
+		return false;
+	int end = x / 2;
+	for (int i = 2; i <= x / 2; i++) {
+		if (x % i == 0)
+			return false;
+	}
+	return true;
+}
