eaa482dd4884c49511a8ba3b3ae4431448e59f7a
dldmldlsy <dldmldlsy@naver.com>
2021-08-02 12:43:23 +0900

    스터디-탐욕법

--- 프로그래머스/프로그래머스_탐욕법_체육복.cpp
+++ 프로그래머스/프로그래머스_탐욕법_체육복.cpp
@@ -6,14 +6,32 @@ using namespace std;
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
     int studentN = n; 
-    vector<int> lost_num = lost;
+    vector<int> lost_num;
     vector<int> reserve_num = reserve; 
-    int count = studentN - lost_num.size(); //현재 체육복을 갖고 있는 학생 수 
+    int count = studentN - lost.size(); //현재 체육복을 갖고 있는 학생 수 
+    cout << count << endl;
+    for (vector<int> ::iterator i = lost.begin(); i < lost.end(); i++) { //안가져온 학생 번호의 배열을 돌면서 
+       // cout << "그냥" << *i << endl;
+        bool self = false;
+
+        for (vector<int> ::iterator iter = reserve_num.begin(); iter < reserve_num.end(); iter++) {
+            if (*i == *iter ) {
+               // cout << *iter << endl;
+                reserve_num.erase(iter); 
+                count++; self = true;
+                break; 
+            }
+        }
+        if (!self) lost_num.push_back(*i);
+       // cout << "!" << endl;
+    }
+   // cout << ": " << lost_num.size() << endl;
     for (int i = 0; i < lost_num.size(); i++) { //안가져온 학생 번호의 배열을 돌면서 
         for (vector<int> ::iterator iter = reserve_num.begin(); iter < reserve_num.end(); iter++) {
             if (lost_num[i] == *iter - 1 || lost_num[i] == *iter + 1) {
-                cout << *iter << endl;
+              // cout << *iter << endl;
                 reserve_num.erase(iter); count++;
+                break;
             }
         }
     }
@@ -21,5 +39,5 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     return answer;
 }
 int main() {
-    cout<<solution(5, { 2,4 }, { 1,3,5 });
+    cout<<"총"<<solution(5, {2,4}, {1,3,5});
 }
