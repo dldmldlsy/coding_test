db17a9b7b03a4b21df364ed43e0a2aa2f2d3f186
dldmldlsy <dldmldlsy@naver.com>
2021-08-03 19:35:44 +0900

    스터디-그리디

---
+++ 프로그래머스/프로그래머스_탐욕법_조이스틱.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+ 
+using namespace std;
+
+int solution(string name) {
+    int answer = 0;
+    int len = name.length();  
+    // a b c d e f g h i j   k l m n o p q r s t   u v w x y z  26 (65~90)
+    for (int i = 0; i < len; i++) {
+        int asci = name[i];
+        if (name[i] == 'A') {
+            continue; 
+        }
+        else if () {
+
+        }
+    }
+    return answer; 
+}
