665012c939854c8e2fba14d096521242e669daca
dldmldlsy <dldmldlsy@naver.com>
2021-08-18 23:57:18 +0900

    프로그래머스-네트워크

---
+++ 프로그래머스/프로그래머스_DFSBFS_네트워크.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+bool check[201];
+void dfs(vector<vector<int>> v, int idx) {
+    for (int i = 0; i < v[idx].size(); i++) {
+        if (v[idx][i] == 1 && check[i] == false) {
+            check[i] = true; 
+            dfs(v, i);
+        }
+    }
+}
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+     
+    for (int i = 0; i < n; i++) {
+        if (check[i] == false) {
+            dfs(computers, i);
+            answer++;
+        }
+    }
+    return answer;
+}
