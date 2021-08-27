f6b901c1b8147ada8fdeb34d8ae3d79de0b4c6f9
dldmldlsy <dldmldlsy@naver.com>
2021-08-27 21:54:18 +0900

    절댓값힙ver2

--- 백준/Boj11286.cpp
+++ 백준/Boj11286.cpp
@@ -1,6 +1,7 @@
 //백준11286번
 //우선순위큐-절댓값힙
-//시간초과ㅠㅠ
+//시간초과ㅠㅠ... 이게 안되는 이유는 뭘까?? 
+//pair로 하면 되는데 이게 안되는 이유부터 찾기. 
 #include<iostream>
 #include<deque>
 #include<algorithm>
@@ -10,6 +11,10 @@ bool cmp(int a, int b) {
 	return abs(a) < abs(b);
 }
 int main() {
+	ios_base::sync_with_stdio(0);
+
+	cin.tie(0);
+
 	int n; 
 	cin >> n; 
 	int tmp;
@@ -21,8 +26,8 @@ int main() {
 			sort(d.begin(), d.end(), cmp);
 		}
 		else {
-			if (d.empty()) cout << 0 << endl;
-			d.pop_front();
+			if (d.empty()) cout << 0 << "\n";
+			else { cout << d.front() << "\n";  d.pop_front(); }
 		}
 	}
 
