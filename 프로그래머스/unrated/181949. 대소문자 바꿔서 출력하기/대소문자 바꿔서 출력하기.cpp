c43b45ba5c577dc91af723aedcd920d08687dfb8
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2023-09-19 15:43:03 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 2.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for(int i=0; i<str.size(); i++){
+        if(isupper(str[i])){
+            str[i]+=32;
+        }else{
+            str[i]-=32;
+        }
+    }
+    cout<<str;
+    return 0;
+}
