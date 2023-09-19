fb71e8f2979f5602194c0919cf42ae26c3184d11
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2023-09-19 15:48:10 +0900

    [unrated] Title: 문자열 돌리기, Time: 1.64 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for(auto s: str){
+        cout<<s<<"\n";
+    }
+    return 0;
+}
