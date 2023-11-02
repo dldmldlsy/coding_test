e845b8622419c065770597b1d7b46f28e0b245e2
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2023-11-02 15:25:30 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -1,22 +1,16 @@
 #include <string>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 vector<string> solution(string my_string) {
     vector<string> answer;
-    string tmp=""; 
-    bool yn = false; 
-    for(int i=0; i<my_string.size(); i++){
-        if(my_string[i]==' '){
-            if(!yn&&!tmp.empty()){ answer.push_back(tmp); tmp = ""; yn = true; } 
-        }else{
-            if(yn) { yn = false; } 
-            tmp += my_string[i];
-        }
-    }
-    if(!tmp.empty()){
-        answer.push_back(tmp); 
-    }
     
+    stringstream ss(my_string); 
+    string word = ""; 
+    while(ss>>word){
+        answer.push_back(word);
+    }
+
     return answer;
 }
