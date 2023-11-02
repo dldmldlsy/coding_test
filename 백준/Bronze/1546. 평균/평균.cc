e496bad2c48586264831b69ed62e29609583a40e
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2023-11-02 21:05:56 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	double* a = new double [n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	double m = a[0];
+	for (int i = 1; i < n; i++) {
+		if (m < a[i]) {
+			m = a[i];
+		}
+	}
+	double sum = 0;
+	for (int i = 0; i < n; i++) {
+		a[i] = a[i]/m *100;
+		sum += a[i];
+	}
+	cout<< sum / n << endl;
+}
