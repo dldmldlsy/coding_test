35fad88d1b656084851ba09706ae00edc53e48f2
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2024-03-13 00:10:10 +0900

    [level 1] Title: 달리기 경주, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.02 ms
+메모리: 4.2 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 12:54:49
+2024년 03월 13일 00:10:05
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
@@ -1,23 +1,42 @@
+//달리기 경주
+//- 바로 앞의 선수 추월 시, 추월한 선수 이름 호명
+//- 경주 끝났을 때, 등수 오름차순
+#include <iostream>
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
 using namespace std;
 
 vector<string> solution(vector<string> players, vector<string> callings) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     vector<string> answer;
-    map<string, int> m; 
-    
-    for(int i=0; i<players.size(); i++){
-        m[players[i]]=i; 
+    map<string, int> m; // m[이름] = 등수
+    map<int, string> m2; //m[등수] = 이름
+    int rank = 1; 
+    for(string s: players) {
+        m[s] = rank;
+        m2[rank] = s;
+        rank++;
     }
     
-    for(string overName: callings){
-        int overNum = m[overName];
-        int backNum = overNum-1; 
-        string backName = players[backNum]; 
-        swap(players[backNum], players[overNum]);
-        swap(m[overName], m[backName]);        
+    for(string s: callings) {
+        int pass_rank = m[s]; //추월한 사람 기존 등수
+        string back_name = m2[pass_rank-1]; //추월 당한 사람의 이름 
+        //등수 바꿈
+        m[s] = pass_rank-1; 
+        m[back_name] = pass_rank;
+        m2[pass_rank-1] = s;
+        m2[pass_rank] = back_name;
     }
-    answer = players;
+    
+    
+    answer.assign(players.size(), "");
+    
+    for(auto iter = m.begin(); iter!=m.end(); iter++) {
+        answer[(*iter).second-1]=(*iter).first;
+    }
+        
     return answer;
 }
