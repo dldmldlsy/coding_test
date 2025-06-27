0a38216fc03b01203dfed49c05babbe67902ea4f
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-06-28 03:19:20 +0900

    [level 0] Title: 접미사 배열, Time: 0.47 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        int len = my_string.length();
+        answer = new String[len];
+        for(int i=0; i<len; i++){
+            answer[i] = my_string.substring(len-1-i);
+        }
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+}
