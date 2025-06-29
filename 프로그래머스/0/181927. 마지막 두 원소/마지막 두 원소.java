3a33532b57a2ad0a2066407fe49b9f5e809452a9
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-06-29 20:31:28 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        int size = num_list.length;
+        answer = new int[size+1];
+        for(int i=0; i<size; i++){
+            answer[i] = num_list[i];
+        }
+        answer[size] = (num_list[size-2]<num_list[size-1])? num_list[size-1]-num_list[size-2] : num_list[size-1]*2; 
+        return answer;
+    }
+}
