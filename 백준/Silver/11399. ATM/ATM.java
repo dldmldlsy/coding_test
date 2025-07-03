6387c4bb0cf400d77f76edb3b7f53ea1d20adaa3
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-07-03 23:47:47 +0900

    [Silver IV] Title: ATM, Time: 208 ms, Memory: 19396 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] times = new int[n];
+        
+        for(int i=0; i<n; i++){
+            times[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(times);
+
+        int sum =0; 
+        int total = 0; 
+        for(int i=0; i<n; i++){
+            sum += times[i];
+            total += sum;
+        }
+        
+        System.out.println(total);
+    }
+}
