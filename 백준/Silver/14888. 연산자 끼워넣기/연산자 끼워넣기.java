f4a13672f798ad47dfc426d61544a9a99a1fd6ff
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-07-12 23:38:18 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 172 ms, Memory: 17776 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,66 +1,44 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    static int[] ops = new int[4];
-    static int[] numbers;
-    static int cnt;
-    static int n;
-    static int result;
+    static int N;
+    static int[] nums;
+    static int[] ops = new int[4]; // + - * /
     static int max = Integer.MIN_VALUE;
     static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        nums = new int[N];
+        for (int i = 0; i < N; i++) nums[i] = sc.nextInt();
+        for (int i = 0; i < 4; i++) ops[i] = sc.nextInt();
 
-        n = sc.nextInt();
-        numbers = new int[n];
-
-        for(int i=0 ;i<n; i++){
-            numbers[i] = sc.nextInt();
-        }
-
-        for(int i=0; i<4; i++){
-            ops[i] = sc.nextInt();
-        }
-        cnt =0;
-        result = 0;
-
-            dfs(cnt, numbers[0], 1);
-
-
+        dfs(1, nums[0]);
         System.out.println(max);
         System.out.println(min);
-
     }
 
-    static void dfs(int cnt, int result, int numberIdx){
-        if(cnt==(n-1)){
-            if(min>result){
-                min = result;
-            }
-            if(max<result){
-                max = result;
-            }
+    static void dfs(int idx, int result) {
+        if (idx == N) {
+            max = Math.max(max, result);
+            min = Math.min(min, result);
             return;
         }
-        int nextResult = 0;
-        for(int i=0 ;i<4; i++){
-            if(ops[i]==0){
-                continue;
-            }else if(i==0){ //plus
-                nextResult = result + numbers[numberIdx];
-            }else if(i==1){ //minus
-                nextResult = result - numbers[numberIdx];
 
-            }else if(i==2){ //multi
-                 nextResult = result * numbers[numberIdx];
+        for (int i = 0; i < 4; i++) {
+            if (ops[i] > 0) {
+                ops[i]--;
 
-            }else if(i==3){ //div
-                nextResult = result / numbers[numberIdx];
+                int next = 0;
+                if (i == 0) next = result + nums[idx];
+                else if (i == 1) next = result - nums[idx];
+                else if (i == 2) next = result * nums[idx];
+                else if (i == 3) next = result / nums[idx];
+
+                dfs(idx + 1, next);
+                ops[i]++;
             }
-            ops[i] -= 1;
-            dfs(cnt+1, nextResult, numberIdx+1);
-            ops[i] += 1;
         }
     }
 }
