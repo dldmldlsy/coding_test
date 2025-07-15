9ff986d2f4256f2e5a765d951063fa151dd2c9e7
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-07-16 00:52:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 512 ms, Memory: 38500 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,74 +1,71 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m, v; 
-    static int[] check; 
     static ArrayList<ArrayList<Integer>> graph;
-    
-    static void dfs(int v){
-        if(check[v]==1){
-            return; 
+    static boolean[] visited;
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int v = sc.nextInt(); // 탐색 시작 정점 번호
+
+        graph = new ArrayList<>();
+        visited = new boolean[n+1];
+
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
         }
-        check[v] = 1; 
+
+        int a, b;
+        for(int i=0; i<m; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        //정점번호 오름차순 정렬
+        for(int i=1; i<=n; i++){
+            Collections.sort(graph.get(i));
+        }
+
+        dfs(v);
+        System.out.println();
+
+        visited = new boolean[n+1];
+        bfs(v);
+
+    }
+
+    static void dfs(int v){
+        if(visited[v]){
+            return;
+        }
+        visited[v]=true;
         System.out.print(v+" ");
-        
         for(int i=0; i<graph.get(v).size(); i++){
-            if(check[graph.get(v).get(i)]==0){
+            if(!visited[graph.get(v).get(i)]){
                 dfs(graph.get(v).get(i));
             }
         }
-        return; 
     }
-    
+
     static void bfs(int v){
-        Queue<Integer> q = new LinkedList<Integer>(); 
-        q.offer(v);
-        check[v] = 1; 
-        int now; 
+        Queue<Integer> q = new LinkedList<>();
+        q.add(v);
+        visited[v] = true;
         while(!q.isEmpty()){
-            now = q.peek(); 
-            System.out.print(now+" "); 
-            q.poll(); 
-            for(int i=0 ; i<graph.get(now).size(); i++){
-                if(check[graph.get(now).get(i)]==0){
-                    q.offer(graph.get(now).get(i));
-                    check[graph.get(now).get(i)] = 1; 
+            int peek = q.remove();
+            System.out.print(peek+" ");
+            for(int i=0; i<graph.get(peek).size(); i++){
+                int now = graph.get(peek).get(i);
+                if(!visited[now]){
+                    q.add(now);
+                    visited[now] = true;
                 }
             }
         }
-    }
-    
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt(); 
-        m = sc.nextInt(); 
-        v = sc.nextInt(); 
-        
-        graph = new ArrayList<>(); 
-        check = new int[n+1]; // idx = 정점 번호가 되도록.
-        for(int i=0; i<=n; i++){
-            graph.add(new ArrayList<Integer>()); 
-        }
-        int a, b; 
-        for(int i=0; i<m; i++){
-            a = sc.nextInt(); 
-            b = sc.nextInt(); 
-            
-            graph.get(a).add(b); 
-            graph.get(b).add(a);
-        }
-        
-        for(int i=1; i<=n; i++){
-            Collections.sort(graph.get(i)); 
-        }
-        
-        dfs(v);
-        System.out.println();
-        
-        Arrays.fill(check, 0); 
-        
-        bfs(v);
     }
 }
