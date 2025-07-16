e77db2b4a66c7e5703fe14cb47e4c949bafe1bc3
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-07-17 00:36:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1888 ms, Memory: 332300 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    static ArrayList<ArrayList<Integer>> graph;
+    static boolean[] visited;
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int u, v;
+
+        graph = new ArrayList<>();
+
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        visited = new boolean[n+1];
+
+        for(int i=0 ; i<m; i++){
+            u = sc.nextInt();
+            v = sc.nextInt();
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        int cnt=0;
+        for(int i=1; i<=n; i++){
+            if(!visited[i]){
+                dfs(i);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    static void dfs(int num){
+        if(visited[num]){
+            return;
+        }
+        visited[num] = true;
+        for(int i=0; i<graph.get(num).size(); i++){
+            if(!visited[graph.get(num).get(i)]){
+                dfs(graph.get(num).get(i));
+            }
+        }
+    }
+}
