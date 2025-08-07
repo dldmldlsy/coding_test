8306d1a9f61e0221ff72581d03b3d78553ed2467
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-08-08 03:50:39 +0900

    [Silver III] Title: 시리얼 번호, Time: 112 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
@@ -1,41 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] serial = new String[n];
+        String str = br.readLine();
+        int n = Integer.parseInt(str);
 
+        String[] numbers = new String[n];
         for(int i=0; i<n; i++){
-            serial[i] = sc.next();
+            numbers[i] = br.readLine();
         }
 
-        Arrays.sort(serial, (s1, s2)->{
-           if(s1.length()!=s2.length()){
-               return s1.length() - s2.length();
-           }else{
-               int sum1 = 0;
-               int sum2 = 0;
-               for(int i=0; i<s1.length(); i++){
-                   if(Character.isDigit(s1.charAt(i))){
-                       sum1+= s1.charAt(i)-'0';
-                   }
-                   if(Character.isDigit(s2.charAt(i))){
-                       sum2+= s2.charAt(i)-'0';
-                   }
-               }
-               if(sum1!=sum2){
-                   return sum1-sum2;
-               }
-           }
-           return s1.compareTo(s2);
+        Arrays.sort(numbers, (g1, g2) -> {
+          if(g1.length() != g2.length()){
+              return g1.length()-g2.length();
+          }else{
+              //길이가 같으면 -> 숫자합
+              int sum1 = 0;
+              int sum2 = 0;
+              for(int i=0; i<g1.length(); i++){
+                  int n1 = g1.charAt(i)-'0';
+                  int n2 = g2.charAt(i)-'0';
+                  if(n1>=0&&n1<=9) {
+                      sum1 += n1;
+                  }
+                  if(n2>=0&&n2<=9) {
+                      sum2 += n2;
+                  }
+              }
+              if(sum1 != sum2){
+                  return sum1-sum2;
+              }else{
+                  //숫자합도 같다면 -> 사전순
+                  return g1.compareTo(g2);
+              }
+          }
         });
 
-
-        for(int i=0; i<n; i++){
-            System.out.println(serial[i]);
+        StringBuilder sb = new StringBuilder();
+        for(String number : numbers){
+            sb.append(number+"\n");
         }
+        System.out.println(sb);
     }
 }
+
+
+
