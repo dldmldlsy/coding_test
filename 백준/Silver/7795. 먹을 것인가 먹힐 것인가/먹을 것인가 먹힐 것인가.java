b3ae5aa0912cd557c9b6a0f44b00257e813fb76c
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-08-08 19:56:25 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 388 ms, Memory: 37312 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -2,51 +2,64 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-	
     public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int T = Integer.parseInt(st.nextToken());
-		for (int t = 1; t <= T; t++) {
-			// 입력
-			st = new StringTokenizer(br.readLine());
-			int Ano = Integer.parseInt(st.nextToken());  // A 수
-			int Bno = Integer.parseInt(st.nextToken());  // B 수
-			
-			int[] A = new int[Ano];
-			int[] B = new int[Bno];
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < A.length; i++) {
-				A[i] = Integer.parseInt(st.nextToken());
-			}
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < B.length; i++) {
-				B[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			// 정렬
-			Arrays.sort(A);
-			Arrays.sort(B);
-			
-			// 투포인터
-			int i = 0;  // A 인덱스
-			int j = 0;  // B 인덱스
-			int cnt = 0;  // 큰 쌍의 개수
-			while (true) {
-				if (i == Ano || j == Bno) break;
-				if (A[i] > B[j]) {
-					cnt += Ano - i;
-					j++;
-				} else {
-					i++;
-				}
-			}
-			System.out.println(cnt);
-		}
-		
-	}
+        // 빠른 입력을 위해 BufferedReader 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 테스트 케이스 개수
+        int t = Integer.parseInt(br.readLine());
+        int n, m; // A 배열 크기, B 배열 크기
+
+        // 테스트 케이스 반복
+        while(t-- > 0){
+            // N과 M 입력
+            String[] str = br.readLine().split(" ");
+            n = Integer.parseInt(str[0]); // A 배열 크기
+            m = Integer.parseInt(str[1]); // B 배열 크기
+
+            // A와 B 배열 선언
+            int[] arr = new int[n];
+            int[] brr = new int[m];
+
+            // A 배열 원소 입력
+            String[] aStr = br.readLine().split(" ");
+            for(int i = 0; i < n; i++){
+                arr[i] = Integer.parseInt(aStr[i]);
+            }
+
+            // B 배열 원소 입력
+            String[] bStr = br.readLine().split(" ");
+            for(int i = 0; i < m; i++){
+                brr[i] = Integer.parseInt(bStr[i]);
+            }
+
+            // B 배열을 오름차순 정렬
+            Arrays.sort(brr);
+            int cnt=0;
+
+            for(int i=0; i<n; i++){
+                cnt+= lowerBound(brr, arr[i]);
+            }
+
+            // 결과 출력
+            System.out.println(cnt);
+        }
+    }
+
+    public static int lowerBound(int[] array, int target){
+        int left = 0;
+        int right = array.length;
+
+        while(left<right){
+            int mid = (left+right)/2;
+            if(array[mid]<target){
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+    }
 }
