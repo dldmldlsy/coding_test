9f85da98b51cb0bef4b91a57a1857d1608f13763
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-08-09 04:19:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.48 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 97.2 MB, 시간: 82.14 ms
+메모리: 97.4 MB, 시간: 77.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 04일 16:45:47
+2025년 08월 09일 04:19:46
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,43 +1,27 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        int prev;
-        //<"이름", 해당 이름 가진 사람 수>
         Map<String, Integer> map = new HashMap<>();
         
-        for(String name : participant){
-            if(map.containsKey(name)) {//이미 있다면
-                prev = map.get(name); //기존값에 
-                map.put(name, prev+1); // +1
-            }else{// 없다면 1로 저장
-                map.put(name, 1); 
-            }
+        for(String partici: participant){
+            map.put(partici, map.getOrDefault(partici, 0)+1);
         }
         
-        //<"이름", 해당 이름 가진 사람 중 완주한 사람 수>
-        Map<String, Integer> map2 = new HashMap<>();
-        
-        for(String name : completion){
-            //해당 키값을 가진 데이터가 있으면 그 값을 가져오고, 없으면 0을 가져옴
-            prev = map2.getOrDefault(name, 0);
-            map2.put(name, prev+1); // 가져온 값에 +1
+        for(String complet: completion){
+            map.put(complet, map.get(complet)-1);
         }
         
-        for(String name: participant){ //참가자 이름 배열 돌면서
-            if(!map2.containsKey(name)){ //완주 이름 목록에 없다면
-                answer = name; //완주하지 못한 선수!
-                break;
-            }
-            
-            //해당 이름을 가진 사람 수랑 해당 이름을 가지고 완주한 사람 수를 비교
-            if(!map.get(name).equals(map2.get(name))){
-                answer = name; // 다르면 완주하지 못한 선수!
+        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry<String, Integer> e = it.next();
+            if(e.getValue()>0){
+                answer = e.getKey();
                 break;
             }
         }
-    
+        
         return answer;
     }
 }
