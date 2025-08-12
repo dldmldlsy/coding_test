7b90948ed79e244e1bf055e2e613128b836e7f09
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-08-12 22:30:24 +0900

    [level 2] Title: 가장 큰 수, Time: 3258.35 ms, Memory: 389 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,24 +3,26 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        String[] strs = new String[numbers.length];
-         
-        for(int i = 0; i<numbers.length; i++){
-            strs[i] = Integer.toString(numbers[i]);
+        Integer[] number = new Integer[numbers.length];
+        for(int i=0; i<number.length; i++){
+            number[i] = numbers[i];
         }
-        //정렬
-        Arrays.sort(strs, (s1, s2)-> {
-               return (s2+s1).compareTo(s1+s2);
+        Arrays.sort(number, (n1, n2)->{
+            String str1 = Integer.toString(n1);
+            String str2 = Integer.toString(n2);
+            String str12 = str1 + str2;
+            String str21 = str2 + str1;
+            
+            return str21.compareTo(str12);
         });
         
-        if(strs[0].equals("0")) {
-            answer = "0"; 
-        }else{
-            for(String str : strs){
-                answer += str;
-            }
+        for(int i=0; i<number.length; i++){
+            answer += Integer.toString(number[i]);
         }
         
+        if(answer.charAt(0)=='0'){
+            answer = "0";
+        }
         return answer;
     }
 }
