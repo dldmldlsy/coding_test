2dc866c85b5d387385964ea36fdcd0c5d97bdf0f
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-09-17 22:38:56 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.67 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int pos = 0;
+        for(int i=0; i<seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                pos = i;
+            }
+        }
+        
+        answer = "김서방은 "+pos+"에 있다";
+        return answer;
+    }
+}
