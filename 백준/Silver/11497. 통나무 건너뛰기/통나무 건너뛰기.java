bcbc78404b7fbaea2dac34267e9ee8157bdeb8ea
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-09-30 00:09:38 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 556 ms, Memory: 55980 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
@@ -39,7 +39,7 @@ public class Main {
             int level = 0;
             for(int i=0; i<list.size(); i++){
                 if(i==list.size()-1){
-                    level = Math.abs(list.get(list.size()-1)-list.get(i));
+                    level = Math.abs(list.get(list.size()-1)-list.get(0));
                 }else{
                     level = Math.abs(list.get(i)-list.get(i+1));
                 }
