74115739153c2387c8e77527a943df7bdd0e932f
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-10-10 23:18:06 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        while(true){
+            String str = br.readLine();
+            if(str!=null){
+                System.out.println(str);
+            }else{
+                break;
+            }
+        }
+    }
+}
