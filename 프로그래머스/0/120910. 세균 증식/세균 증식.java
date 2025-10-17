04d9347c881a7518882e9430167e4401a167fae6
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-10-18 01:10:42 +0900

    [level 0] Title: 세균 증식, Time: 0.05 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(int n, int t) {
         int answer = n;
-        while(t-->0){
-            answer *= 2;
-        }
+        answer *= Math.pow(2, t);
         return answer;
     }
 }
