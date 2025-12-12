f1dfe01a5f6af11daeac135425c44c021dc5c764
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-12-13 00:00:09 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 9.09 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -0,0 +1,12 @@
+def solution(emergency):
+    arr = []
+    
+    for i in emergency:
+        idx = 1
+        
+        for j in emergency:
+            if i < j:
+                idx += 1
+        arr.append(idx)
+        
+    return arr
