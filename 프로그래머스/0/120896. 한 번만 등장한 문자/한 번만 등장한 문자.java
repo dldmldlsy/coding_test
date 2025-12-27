f0cdbaed7744cb38a9a5b99d295614a95171b11f
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2025-12-28 00:10:17 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 14.98 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int[] arr = new int[26];
+        
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            arr[c-97]++;
+            
+        }
+        
+        for(int i=0; i<26; i++){
+            if(arr[i]==1){
+                answer+=(char)(i+97);
+            }
+        }
+        return answer;
+    }
+}
