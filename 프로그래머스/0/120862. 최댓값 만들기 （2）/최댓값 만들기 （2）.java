c42f0bf6335f961363b405a41e5b0e10f3424dd3
Eui in Lee <76683396+dldmldlsy@users.noreply.github.com>
2026-01-29 22:37:12 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 1.22 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        Arrays.sort(numbers);
+        
+        int len = numbers.length;
+        
+        answer = Math.max(numbers[len-1]*numbers[len-2], numbers[0]*numbers[1]);
+        return answer;
+    }
+}
